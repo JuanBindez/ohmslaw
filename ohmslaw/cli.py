2de52c1f39f142e65d02c7e0f6ea35fed928c9af
JuanBindez <juanbindez780@gmail.com>
2024-05-16 10:31:13 -0300

    update

--- ohmslaw/cli.py
+++ ohmslaw/cli.py
@@ -1,53 +1,69 @@
 import argparse
-
 from ohmslaw.__main__ import Ohms
 
-class OhmsCLI:
-    """Command line interface for Ohms calculations."""
 
-    def __init__(self):
-        self.parser = argparse.ArgumentParser(description='Perform electrical calculations based on Ohm\'s Law')
-        self.subparsers = self.parser.add_subparsers(dest='operation', help='Operation to perform')
+def calculate_volts(I, R):
+    """Calculate voltage using Ohm's Law."""
+    ohms = Ohms()
+    return ohms.volts(I, R)
 
-        # Subparser for volts operation
-        volts_parser = self.subparsers.add_parser('volts', help='Calculate voltage using Ohm\'s Law')
-        volts_parser.add_argument('-I', type=float, required=True, help='Current in amperes')
-        volts_parser.add_argument('-R', type=float, required=True, help='Resistance in ohms')
+def calculate_current(V, R):
+    """Calculate current using Ohm's Law."""
+    ohms = Ohms()
+    return ohms.current(V, R)
 
-        # Subparser for current operation
-        current_parser = self.subparsers.add_parser('current', help='Calculate current using Ohm\'s Law')
-        current_parser.add_argument('-V', type=float, required=True, help='Voltage in volts')
-        current_parser.add_argument('-R', type=float, required=True, help='Resistance in ohms')
+def calculate_resistance(V, I):
+    """Calculate resistance using Ohm's Law."""
+    ohms = Ohms()
+    return ohms.resistance(V, I)
 
-        # Subparser for resistance operation
-        resistance_parser = self.subparsers.add_parser('resistance', help='Calculate resistance using Ohm\'s Law')
-        resistance_parser.add_argument('-V', type=float, required=True, help='Voltage in volts')
-        resistance_parser.add_argument('-I', type=float, required=True, help='Current in amperes')
+def find_resistor(source, component_voltage, component_current=0.02):
+    """Calculate resistor value to limit current."""
+    ohms = Ohms()
+    return ohms.find_resistor(source, component_voltage, component_current)
 
-        # Subparser for find_resistor operation
-        find_resistor_parser = self.subparsers.add_parser('find_resistor', help='Calculate resistor value to limit current')
-        find_resistor_parser.add_argument('--source', type=float, required=True, help='Source voltage in volts')
-        find_resistor_parser.add_argument('--component_voltage', type=float, required=True, help='Component voltage in volts')
-        find_resistor_parser.add_argument('--component_current', type=float, default=0.02, help='Component current in amperes (default: 0.02)')
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Perform electrical calculations based on Ohm's Law")
+    subparsers = parser.add_subparsers(dest='operation', help='Operation to perform')
 
-    def main(self):
-        args = self.parser.parse_args()
-        ohms = Ohms()
+    # Subparser for volts operation
+    volts_parser = subparsers.add_parser('volts', help='Calculate voltage using Ohm\'s Law')
+    volts_parser.add_argument('-I', type=float, required=True, help='Current in amperes')
+    volts_parser.add_argument('-R', type=float, required=True, help='Resistance in ohms')
 
-        if args.operation == 'volts':
-            result = ohms.volts(args.I, args.R)
-        elif args.operation == 'current':
-            result = ohms.current(args.V, args.R)
-        elif args.operation == 'resistance':
-            result = ohms.resistance(args.V, args.I)
-        elif args.operation == 'find_resistor':
-            result = ohms.find_resistor(args.source, args.component_voltage, args.component_current)
+    # Subparser for current operation
+    current_parser = subparsers.add_parser('current', help='Calculate current using Ohm\'s Law')
+    current_parser.add_argument('-V', type=float, required=True, help='Voltage in volts')
+    current_parser.add_argument('-R', type=float, required=True, help='Resistance in ohms')
 
-        print("Result:", result)
+    # Subparser for resistance operation
+    resistance_parser = subparsers.add_parser('resistance', help='Calculate resistance using Ohm\'s Law')
+    resistance_parser.add_argument('-V', type=float, required=True, help='Voltage in volts')
+    resistance_parser.add_argument('-I', type=float, required=True, help='Current in amperes')
+
+    # Subparser for find_resistor operation
+    find_resistor_parser = subparsers.add_parser('find_resistor', help='Calculate resistor value to limit current')
+    find_resistor_parser.add_argument('--source', type=float, required=True, help='Source voltage in volts')
+    find_resistor_parser.add_argument('--component_voltage', type=float, required=True, help='Component voltage in volts')
+    find_resistor_parser.add_argument('--component_current', type=float, default=0.02, help='Component current in amperes (default: 0.02)')
+
+    args = parser.parse_args()
+
+    if args.operation == 'volts':
+        result = calculate_volts(args.I, args.R)
+    elif args.operation == 'current':
+        result = calculate_current(args.V, args.R)
+    elif args.operation == 'resistance':
+        result = calculate_resistance(args.V, args.I)
+    elif args.operation == 'find_resistor':
+        result = find_resistor(args.source, args.component_voltage, args.component_current)
+
+    print("Result:", result)
 
 if __name__ == "__main__":
-    cli = OhmsCLI()
-    cli.main()
+    main()
+
 
 """
     python cli.py volts -I 2 -R 4
