6fc9330c8c524022ed1816f9adc574bea02dbb25
JuanBindez <juanbindez780@gmail.com>
2024-05-15 23:02:03 -0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ohmslaw import Ohms
 
 o = Ohms()
-results = o.resistance()
+results = o.resistance(V=48, I=4)
 
 print(results)
--- ohmslaw/__init__.py
+++ ohmslaw/__init__.py
@@ -1,1 +1,7 @@
+__title__ = "ohmslaw"
+__author__ = "Juan Bindez"
+__license__ = "GPLv2 License"
+__js__ = None
+__js_url__ = None
+
 from ohmslaw.__main__ import Ohms
