8abffca2edc1d2f1cd21dec590e372c698230df9
JuanBindez <juanbindez780@gmail.com>
2024-06-29 08:30:39 -0300

    Fix: Correct calculation for best combination of resistors
    - Fixed the current calculation to properly account for the total resistance.
    - Adjusted the voltage drop calculation to correctly determine the voltage across the component.
    - Ensured the resulting voltage matches the desired component voltage.
    - Verified functionality with example values, producing the correct resistor combination and voltage output.

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 git add .
-git commit -m 'ohmslaw 1.3.1 -> Fix: Correct calculation for best combination of resistors
+git commit -m 'Fix: Correct calculation for best combination of resistors
 
 - Fixed the current calculation to properly account for the total resistance.
 - Adjusted the voltage drop calculation to correctly determine the voltage across the component.
 - Ensured the resulting voltage matches the desired component voltage.
 - Verified functionality with example values, producing the correct resistor combination and voltage output.
 '
-git push -u origin dev
+git push -u origin main
 git tag v1.3.1
 git push --tag
 make clean
