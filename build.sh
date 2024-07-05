0052e8e094ccddc88c27367b714b35a0e9c9c204
JuanBindez <juanbindez780@gmail.com>
2024-07-05 11:47:13 -0300

    ohmslaw 2.0.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'removed the "find_resistor" and "best_combinations" methods'
-git push -u origin dev
-git tag v2.0-rc2
+git commit -m 'ohmslaw 2.0.0'
+git push -u origin main
+git tag v2.0.0
 git push --tag
 make clean
 make upload
