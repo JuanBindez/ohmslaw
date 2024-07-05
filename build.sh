7633ac2e6f302d1d82c19e05cb0cb631b29b9e25
JuanBindez <juanbindez780@gmail.com>
2024-07-05 14:09:13 -0300

    ohmslaw 2.1.2

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'ohmslaw 2.1.1'
+git commit -m 'ohmslaw 2.1.2'
 git push -u origin main
-git tag v2.1.1
+git tag v2.1.2
 git push --tag
 make clean
 make upload
