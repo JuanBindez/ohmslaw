04ccef58b9c2868ea9b6b4a1b08829ef329cba0d
JuanBindez <juanbindez780@gmail.com>
2024-07-05 11:39:52 -0300

    removed the "find_resistor" and "best_combinations" methods

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 git add .
 git commit -m 'removed the "find_resistor" and "best_combinations" methods'
-git push -u origin main
-git tag v2.0-rc1
+git push -u origin dev
+git tag v2.0-rc2
 git push --tag
 make clean
 make upload
